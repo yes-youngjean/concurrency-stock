2bc6a11398ca571ed07abafcb45932385231a146
youngjean <youngjean@youngjeanui-MacBookPro.local>
2025-07-30 23:14:59 +0900

    start concurrency-stock project

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,31 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    //기본 생성자
+    public Stock() {
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+}
---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,9 @@
+package com.example.stock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+
+    
+}

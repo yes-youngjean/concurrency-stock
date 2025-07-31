6aa5dd8b38824523c96065c2cbc24497983308a7
youngjean <yeszerojean@gmail.com>
2025-07-31 19:52:25 +0900

    Feat: Implement inventory reduction logic (* possible race condition issue)

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -36,5 +40,30 @@ class StockServiceTest {
         // 100 - 1 => 99개 남아야 함
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99, stock.getQuantity());
+    }
+
+
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(100);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(()-> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertNotEquals(0, stock.getQuantity());
+        /**
+         * Thread1이 값을 가져가 갱신중인데, Thread2도 동일한 값을 가져가 갱신하며 동시성 문제 발생
+         * */
     }
 }
